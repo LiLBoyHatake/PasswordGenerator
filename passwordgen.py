2f6390a56a1541e5dcae031105a188a67fc337f4
LiLBoyHatake <75223711+LiLBoyHatake@users.noreply.github.com>
2020-11-29 20:13:17 -0500

    Working on savingfunctions

--- passwordgen.py
+++ passwordgen.py
@@ -4,15 +4,19 @@ import random
 #define chars
 characters = 'abcdefghijklmnopqrstuvwyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*'
 
+
+
 #Define
 pig = input('Please input the number of password you want...')
 pig = int(pig)
 length = input('How long do you want the password to be?')
 length = int(length)
 
-
 for p in range(pig):
     password = ''
     for c in range(length):
         password += random.choice(characters)
     print(password)
+
+with open('file.txt', 'w') as myFile:
+    file.write(password)
