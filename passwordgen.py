606259c0cc876954aaa8d1c82963dc22d6353ad8
LiLBoyHatake <75223711+LiLBoyHatake@users.noreply.github.com>
2020-11-29 19:45:11 -0500

    Update passwordgen.py

--- passwordgen.py
+++ passwordgen.py
@@ -4,14 +4,14 @@ import random
 #define chars
 characters = 'abcdefghijklmnopqrstuvwyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*'
 
-
-number = input('Please input the number of password you want...')
-number = int(number)
+#Define
+pig = input('Please input the number of password you want...')
+pig = int(pig)
 length = input('How long do you want the password to be?')
 length = int(length)
 
 
-for p in range(number):
+for p in range(pig):
     password = ''
     for c in range(length):
         password += random.choice(characters)
