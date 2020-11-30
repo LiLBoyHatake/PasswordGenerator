1cea15d6bbc70eb5842ca77165a5f5c7bfb6e3b3
LiLBoyHatake <75223711+LiLBoyHatake@users.noreply.github.com>
2020-11-29 20:29:11 -0500

    Fix, only makes one password now

--- passwordgen.py
+++ passwordgen.py
@@ -7,16 +7,12 @@ characters = 'abcdefghijklmnopqrstuvwyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%
 
 
 #Define
-pig = input('Please input the number of password you want...')
-pig = int(pig)
 length = input('How long do you want the password to be?')
 length = int(length)
 
-for p in range(pig):
-    password = ''
-    for c in range(length):
-        password += random.choice(characters)
+for c in range(length):
+    password += random.choice(characters)
     print(password)
 
 with open('file.txt', 'a') as myFile:
-    myFile.write(password', ')
+    myFile.write(password)
